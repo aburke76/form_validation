277b5e0fa4e2eec632996f249ab15939767c22cc
aburke76 <alexlburke8771@gmail.com>
2024-05-27 10:11:29 -0400

    added postcode npm package

--- countries.js
+++ countries.js
@@ -1,4 +1,4 @@
-const countryList = [
+export const countryList = [
     "Afghanistan",
     "Albania",
     "Algeria",
@@ -205,55 +205,3 @@ const countryList = [
     "Zambia",
     "Zimbabwe",
 ];
-
-const email = document.getElementById("email");
-const country = document.getElementById("country");
-const zip = document.getElementById("zip");
-const password = document.getElementById("password");
-const confirm = document.getElementById("confirm-pword");
-const submit = document.getElementById("submit-btn");
-const form = document.querySelector("form");
-const emailError = document.getElementById("email-error");
-const countryError = document.getElementById("country-error");
-const zipError = document.getElementById("zip-error");
-
-email.addEventListener("input", () => {
-    const format = /[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+/;
-    if (!email.value.match(format)) {
-        emailError.textContent =
-            "Enter email in correct format (johndoe@gmail.com)";
-    } else {
-        emailError.textContent = "";
-    }
-});
-
-country.addEventListener("input", () => {
-    const loc = country.value;
-    if (!countryList.includes(loc)) {
-        country.setCustomValidity("invalid");
-        countryError.textContent = "Please enter valid country";
-    } else {
-        country.setCustomValidity("");
-        countryError.textContent = "";
-    }
-});
-
-zip.addEventListener("input", () => {
-    const format = /[0-9]{5}/;
-    if (!zip.value.match(format)) {
-        zip.setCustomValidity("invalid");
-        zipError.textContent = "Enter exactly 5 numbers";
-    } else {
-        zip.setCustomValidity("");
-        zipError.textContent = "";
-    }
-});
-
-form.addEventListener("submit", (event) => {
-    if (!email.value) {
-        emailError.textContent = "Please enter an email address";
-    } else if (!email.validity.valid) {
-        emailError.textContent = "";
-    }
-    event.preventDefault();
-});
